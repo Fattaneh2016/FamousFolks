8962f4871630f740d2453d5392af0d64e410ac15
Fattaneh2016 <fatameri@hotmail.com>
2017-02-12 00:26:35 -0500

    asp.net mvc

---
+++ WebFamousFolks/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebFamousFolks.Startup))]
+namespace WebFamousFolks
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
